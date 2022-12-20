90151320f514d1e91557e410d09049b9dd79fa2a
yamaishi <yamakios8@icloud.com>
2022-12-20 14:50:04 +0900

    devise.rb編集

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -24,6 +24,8 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
+  require 'dotenv'
+  Dotenv.load
   config.mailer_sender = ENV['USER_NAME'] #'please-change-me-at-config-initializers-devise@example.com'
 
   # Configure the class responsible to send e-mails.
