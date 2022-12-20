af396685917ea62a125292f3e28a01a35f87cfd0
yamaishi <yamakios8@icloud.com>
2022-12-20 13:04:30 +0900

    メール環境を変更

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,5 +1,5 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: ENV['MY_ADDRESS']#ENV['USER_NAME'] #
+  default from: ENV['USER_NAME'] #ENV['MY_ADDRESS']
   #default from: Rails.application.credentials.gmail[:user_name]
   layout 'mailer'
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -28,7 +28,7 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.asset_host = 'http://assets.example.com'
@@ -93,7 +93,7 @@ Rails.application.configure do
   #   :user_name => 'apikey',
   #   :password => ENV['SENDGRID_API_KEY'],
   #   :authentication => :plain,
-  #   :enable_starttls_auto => true
+  #  :enable_starttls_auto => true
   # }
 
     config.action_mailer.raise_delivery_errors = true
