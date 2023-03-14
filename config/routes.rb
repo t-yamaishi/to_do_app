d669cd2f6cd5990e67bc636c0fbcb595a0155034
yamaishi <yamakios8@icloud.com>
2023-03-14 17:10:21 +0900

    linebot_controller.rbを削除

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   root 'top#index'
   get 'users/line_disconnect'
   get 'posts/month_calendar'
-  post '/callback' => 'linebot#callback'
   resources :tags, only:[:index, :edit, :create, :update, :destroy]
   resources :posts, only:[:index, :edit, :create, :update, :destroy]
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
